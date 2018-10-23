3d4dc932064065c4e32f73f1d92222160bbe3850
QiuYongJie <443460997@qq.com>
2018-10-23 12:37:12 +0800

    'water'

---
+++ C++/628.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        return fun(nums);
+    }
+    int fun(vector<int>& a) {
+        sort(a.begin(), a.end());
+        int n = a.size();
+        int u = a[0] * a[1] * a[n - 1];
+        int v = a[n - 1] * a[n - 2] * a[n - 3];
+        return max(u, v);
+    }
+};
